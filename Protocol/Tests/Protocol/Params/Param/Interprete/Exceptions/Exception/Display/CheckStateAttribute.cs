30f6d8b5418afb71b36e916293c1484158ae5d49
Robbie Demaegdt <robbie.demaegdt@hotmail.com>
2024-02-07 08:47:25 +0100

    Added Exception Disabled check (#11)
    * Added Exception Disabled check
    
    * Fix remarks
    
    * Small tweaks
    
    * Used EnumDisplayStateConverter
    
    * Added null check
    
    ---------
    
    Co-authored-by: Michiel Oda <97458010+MichielOda@users.noreply.github.com>

---
+++ Protocol/Tests/Protocol/Params/Param/Interprete/Exceptions/Exception/Display/CheckStateAttribute.cs
@@ -0,0 +1,161 @@
+namespace Skyline.DataMiner.CICD.Validators.Protocol.Tests.Protocol.Params.Param.Interprete.Exceptions.Exception.Display.CheckStateAttribute
+{
+    using System;
+    using System.Collections.Generic;
+    using Skyline.DataMiner.CICD.Models.Protocol.Edit;
+    using Skyline.DataMiner.CICD.Models.Protocol.Enums;
+    using Skyline.DataMiner.CICD.Models.Protocol.Read;
+    using Skyline.DataMiner.CICD.Validators.Common.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Common.Model;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common.Attributes;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common.Extensions;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Generic;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Interfaces;
+
+    [Test(CheckId.CheckStateAttribute, Category.Param)]
+    internal class CheckStateAttribute : IValidate, ICodeFix/*, ICompare*/
+    {
+        public List<IValidationResult> Validate(ValidatorContext context)
+        {
+            List<IValidationResult> results = new List<IValidationResult>();
+
+            foreach (IParamsParam param in context.EachParamWithValidId())
+            {
+                if (param.Interprete?.Exceptions == null)
+                {
+                    continue;
+                }
+
+                foreach (var exception in param.Interprete.Exceptions)
+                {
+                    if (exception.Display == null)
+                    {
+                        continue;
+                    }
+
+                    var state = exception.Display.State;
+                    (GenericStatus status, string rawDisplayValue, var stateValue) = GenericTests.CheckBasics(state, isRequired: true);
+
+                    // Missing
+                    if (status.HasFlag(GenericStatus.Missing))
+                    {
+                        results.Add(Error.MissingAttribute(this, param, exception.Display, param.Id?.RawValue).WithExtraData(ExtraData.ExceptionToFix, exception));
+                        continue;
+                    }
+
+                    // Empty
+                    if (status.HasFlag(GenericStatus.Empty))
+                    {
+                        results.Add(Error.EmptyAttribute(this, param, state, param.Id?.RawValue).WithExtraData(ExtraData.ExceptionToFix, exception));
+                        continue;
+                    }
+
+                    // Invalid
+                    if (status.HasFlag(GenericStatus.Invalid))
+                    {
+                        results.Add(Error.InvalidAttributeValue(this, param, state, rawDisplayValue, param.Id?.RawValue).WithExtraData(ExtraData.ExceptionToFix, exception));
+                        continue;
+                    }
+
+                    if (stateValue == EnumDisplayState.Enabled)
+                    {
+                        results.Add(Error.UnrecommendedEnabledValue(this, param, state, param.Id?.RawValue).WithExtraData(ExtraData.ExceptionToFix, exception));
+                        continue;
+                    }
+
+                    // Untrimmed
+                    if (status.HasFlag(GenericStatus.Untrimmed))
+                    {
+                        results.Add(Error.UntrimmedAttributeValue(this, param, state, param.Id?.RawValue, rawDisplayValue).WithExtraData(ExtraData.ExceptionToFix, exception));
+                        continue;
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public ICodeFixResult Fix(CodeFixContext context)
+        {
+            CodeFixResult result = new CodeFixResult();
+            if (!(context.Result.ReferenceNode is IParamsParam readParam))
+            {
+                result.Message = "context.Result.Node is not of type IParamsParam";
+                return result;
+            }
+
+            var editParam = context.Protocol?.Params?.Get(readParam);
+            if (editParam == null)
+            {
+                result.Message = "editParam is null.";
+                return result;
+            }
+
+            if (!context.Result.ExtraData.TryGetValue(ExtraData.ExceptionToFix, out object o)
+                || !(o is IParamsParamInterpreteExceptionsException readException))
+            {
+                result.Message = "ExtraData[exceptionToFix] is not of type IParamsParamInterpreteExceptionsException";
+                return result;
+            }
+
+            var editException = editParam.Interprete?.Exceptions?.Get(readException);
+            if (editException == null)
+            {
+                result.Message = "editException is null.";
+                return result;
+            }
+
+            if (editException.Display == null)
+            {
+                result.Message = "editException.Display is null.";
+                return result;
+            }
+
+            switch (context.Result.ErrorId)
+            {
+                case ErrorIds.EmptyAttribute:
+                case ErrorIds.UnrecommendedEnabledValue:
+                case ErrorIds.InvalidAttributeValue:
+                    editException.Display.State.Value = EnumDisplayState.Disabled;
+                    result.Success = true;
+                    break;
+
+                case ErrorIds.MissingAttribute:
+                    editException.Display.State = new ParamsParamInterpreteExceptionsExceptionDisplayState(EnumDisplayState.Disabled);
+                    result.Success = true;
+                    break;
+
+                case ErrorIds.UntrimmedAttributeValue:
+                    EnumDisplayState? parsedValue = EnumDisplayStateConverter.Convert(editException.Display.State.RawValue.Trim());
+                    if (parsedValue == null)
+                    {
+                        result.Message = "Invalid parsed value.";
+                        return result;
+                    }
+
+                    editException.Display.State.Value = parsedValue;
+                    result.Success = true;
+                    break;
+
+                default:
+                    result.Message = $"This error ({context.Result.ErrorId}) isn't implemented.";
+                    break;
+            }
+
+            return result;
+        }
+
+        ////public List<IValidationResult> Compare(MajorChangeCheckContext context)
+        ////{
+        ////    List<IValidationResult> results = new List<IValidationResult>();
+        ////
+        ////    return results;
+        ////}
+    }
+
+    internal enum ExtraData
+    {
+        ExceptionToFix,
+    }
+}
---
+++ ProtocolTests/Protocol/Params/Param/Interprete/Exceptions/Exception/Display/CheckStateAttribute/CheckStateAttribute.cs
@@ -0,0 +1,304 @@
+namespace ProtocolTests.Protocol.Params.Param.Interprete.Exceptions.Exception.Display.CheckStateAttribute
+{
+    using System;
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    using Skyline.DataMiner.CICD.Validators.Common.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Common.Model;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Tests.Protocol.Params.Param.Interprete.Exceptions.Exception.Display.CheckStateAttribute;
+
+    [TestClass]
+    public class Validate
+    {
+        private readonly IValidate check = new CheckStateAttribute();
+
+        #region Valid Checks
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_Valid()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Valid,
+                FileName = "Valid",
+                ExpectedResults = new List<IValidationResult>()
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        #endregion
+
+        #region Invalid Checks
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_UnrecommendedEnabledValue()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "UnrecommendedEnabledValue",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.UnrecommendedEnabledValue(null, null, null, "100"),
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_MissingAttribute()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "MissingAttribute",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.MissingAttribute(null, null, null, "100"),
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_EmptyAttribute()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "EmptyAttribute",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.EmptyAttribute(null, null, null, "100"),
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_InvalidAttributeValue()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "InvalidAttributeValue",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.InvalidAttributeValue(null, null, null, "true", "100"),
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_UntrimmedAttributeValue()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "UntrimmedAttributeValue",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.UntrimmedAttributeValue(null, null, null, "100", "disabled "),
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        #endregion
+    }
+
+    [TestClass]
+    public class CodeFix
+    {
+        private readonly ICodeFix check = new CheckStateAttribute();
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_UnrecommendedEnabledValue()
+        {
+            Generic.FixData data = new Generic.FixData
+            {
+                FileNameBase = "UnrecommendedEnabledValue",
+            };
+
+            Generic.Fix(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_EmptyAttribute()
+        {
+            Generic.FixData data = new Generic.FixData
+            {
+                FileNameBase = "EmptyAttribute",
+            };
+
+            Generic.Fix(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_MissingAttribute()
+        {
+            Generic.FixData data = new Generic.FixData
+            {
+                FileNameBase = "MissingAttribute",
+            };
+
+            Generic.Fix(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_InvalidAttributeValue()
+        {
+            Generic.FixData data = new Generic.FixData
+            {
+                FileNameBase = "InvalidAttributeValue",
+            };
+
+            Generic.Fix(check, data);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_UntrimmedAttributeValue()
+        {
+            Generic.FixData data = new Generic.FixData
+            {
+                FileNameBase = "UntrimmedAttributeValue",
+            };
+
+            Generic.Fix(check, data);
+        }
+    }
+
+    [TestClass]
+    public class ErrorMessages
+    {
+        [TestMethod]
+        public void Param_CheckStateAttribute_UnrecommendedEnabledValue()
+        {
+            // Create ErrorMessage
+            var message = Error.UnrecommendedEnabledValue(null, null, null, "paramId");
+                        
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Warning,
+                Certainty = Certainty.Uncertain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Exception with state 'enabled'. Param 'paramId'.",
+                Details = "Default behavior is that the state attribute of an exception should be 'disabled'.",
+                HasCodeFix = true,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_EmptyAttribute()
+        {
+            // Create ErrorMessage
+            var message = Error.EmptyAttribute(null, null, null, "paramId");
+
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Warning,
+                Certainty = Certainty.Certain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Empty attribute 'state' in Param 'paramId'.",
+                Details = "Default behavior is that the state attribute of an exception should be 'disabled'.",
+                HasCodeFix = true,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_MissingAttribute()
+        {
+            // Create ErrorMessage
+            var message = Error.MissingAttribute(null, null, null, "paramId");
+
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Warning,
+                Certainty = Certainty.Uncertain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Missing attribute 'state' in Param 'paramId'.",
+                Details = "Default behavior is that the state attribute of an exception should be 'disabled'.",
+                HasCodeFix = true,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_InvalidAttributeValue()
+        {
+            // Create ErrorMessage
+            var message = Error.InvalidAttributeValue(null, null, null, null, "paramId");
+
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Warning,
+                Certainty = Certainty.Certain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Invalid value '' in attribute 'Display@state'. Param ID 'paramId'.",
+                Details = "Default behavior is that the state attribute of an exception should be 'disabled'.",
+                HasCodeFix = true,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_UntrimmedAttributeValue()
+        {
+            // Create ErrorMessage
+            var message = Error.UntrimmedAttributeValue(null, null, null, "paramId", null);
+
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Warning,
+                Certainty = Certainty.Certain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Untrimmed attribute 'Display@state' in Param 'paramId'. Current value ''.",
+                Details = "Default behavior is that the state attribute of an exception should be 'disabled'.",
+                HasCodeFix = true,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+    }
+
+    [TestClass]
+    public class Attribute
+    {
+        private readonly IRoot check = new CheckStateAttribute();
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_CheckCategory() => Generic.CheckCategory(check, Category.Param);
+
+        [TestMethod]
+        public void Param_CheckStateAttribute_CheckId() => Generic.CheckId(check, CheckId.CheckStateAttribute);
+    }
+}
