a05c8838c3354624836385a2b190f64937f55656
Jan Staelens <jan.staelens@skyline.be>
2024-05-13 13:06:37 +0200

    Added tests. Currently failing.

---
+++ ProtocolTests/Protocol/QActions/QAction/CSharpCoreInterAppBrokerSupport/Samples/Validate/Invalid/InvalidInterAppReplyLogic/QAction_1/QAction_1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
+using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+using Skyline.DataMiner.Scripting;
+
+/// <summary>
+/// DataMiner QAction Class: ProcessInterAppReceived.
+/// </summary>
+public class QAction
+{
+	/// <summary>
+	/// The QAction entry point.
+	/// </summary>
+	/// <param name="protocol">Link with SLProtocol process.</param>
+	public static void Run(SLProtocolExt protocol)
+	{
+		try
+		{
+			string raw = Convert.ToString(protocol.GetParameter(protocol.GetTriggerParameter()));
+			IInterAppCall receivedCall = InterAppCallFactory.CreateFromRaw(raw, new List<Type> { });
+			if (receivedCall == null)
+			{
+				protocol.Log("QA" + protocol.QActionID + "|Run|ERR: Value in Parameter was empty.", LogType.Error, LogLevel.NoLogging);
+				return;
+			}
+
+			foreach (var message in receivedCall.Messages)
+			{
+				Message returnMessage;
+				message.TryExecute(protocol, protocol, new Dictionary<Type, Type>(), out returnMessage);
+				if (returnMessage != null)
+				{
+					returnMessage.Send(
+						protocol.SLNet.RawConnection,
+						message.ReturnAddress.AgentId,
+						message.ReturnAddress.ElementId,
+						message.ReturnAddress.ParameterId, new List<Type> { });
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			protocol.Log("QA" + protocol.QActionID + "|" + protocol.GetTriggerParameter() + "|Run|Exception thrown:" + Environment.NewLine + ex, LogType.Error, LogLevel.NoLogging);
+		}
+	}
+}
