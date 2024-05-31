dc96e0202224a933f47259ae4a6ac5825a06f8b7
Pedro Debevere <97611683+PedroDebevere@users.noreply.github.com>
2024-05-31 15:36:18 +0200

    Validator check for detecting Protocol@baseFor equal to Protocol/ElementType (#56)
    * Implementation task DCP233110
    
    * Small cleanup
    
    ---------
    
    Co-authored-by: Michiel Oda <michiel.oda@skyline.be>

---
+++ ProtocolTests/Protocol/CheckBaseForAttribute/CheckBaseForAttribute.cs
@@ -0,0 +1,119 @@
+namespace ProtocolTests.Protocol.CheckBaseForAttribute
+{
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Skyline.DataMiner.CICD.Validators.Common.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Common.Model;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Tests.Protocol.CheckBaseForAttribute;
+
+    [TestClass]
+    public class Validate
+    {
+        private readonly IValidate check = new CheckBaseForAttribute();
+
+        #region Valid Checks
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_Valid()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Valid,
+                FileName = "Valid",
+                ExpectedResults = new List<IValidationResult>()
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_ValidNoBaseForAttribute()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Valid,
+                FileName = "ValidNoBaseForAttr",
+                ExpectedResults = new List<IValidationResult>()
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_ValidNoElementType()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Valid,
+                FileName = "ValidNoElementType",
+                ExpectedResults = new List<IValidationResult>()
+            };
+
+            Generic.Validate(check, data);
+        }
+        #endregion
+
+        #region Invalid Checks
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_InvalidAttribute()
+        {
+            Generic.ValidateData data = new Generic.ValidateData
+            {
+                TestType = Generic.TestType.Invalid,
+                FileName = "InvalidAttribute",
+                ExpectedResults = new List<IValidationResult>
+                {
+                    Error.InvalidAttribute(null, null, null, "Optical Receiver")
+                }
+            };
+
+            Generic.Validate(check, data);
+        }
+
+        #endregion
+    }
+
+    [TestClass]
+    public class ErrorMessages
+    {
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_InvalidAttribute()
+        {
+            // Create ErrorMessage
+            var message = Error.InvalidAttribute(null, null, null, "attributeValue");
+                        
+            var expected = new ValidationResult
+            {
+                Severity = Severity.Critical,
+                Certainty = Certainty.Certain,
+                FixImpact = FixImpact.NonBreaking,
+                GroupDescription = "",
+                Description = "Invalid value 'attributeValue' in attribute 'baseFor'.",
+                Details = "The Protocol@baseFor attribute value must not be equal to the value specified in Protocol/ElementType.",
+                HasCodeFix = false,
+            };
+
+            // Assert
+            message.Should().BeEquivalentTo(expected, Generic.ExcludePropertiesForErrorMessages);
+        }
+    }
+
+    [TestClass]
+    public class Attribute
+    {
+        private readonly IRoot check = new CheckBaseForAttribute();
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_CheckCategory() => Generic.CheckCategory(check, Category.Protocol);
+
+        [TestMethod]
+        public void Protocol_CheckBaseForAttribute_CheckId() => Generic.CheckId(check, CheckId.CheckBaseForAttribute);
+    }
+}
