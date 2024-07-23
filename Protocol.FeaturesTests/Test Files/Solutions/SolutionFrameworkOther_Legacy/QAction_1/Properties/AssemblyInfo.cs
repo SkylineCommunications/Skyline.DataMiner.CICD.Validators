27576cc9fe578806b076144582dfc2383fd34cbf
Michiel Oda <michiel.oda@skyline.be>
2024-07-23 11:28:14 +0200

    Fixed Feature Check for .NET Framework version on legacy projects
    Added legacy projects to the unit tests.
    Fixed condition in feature check to take in account the different syntax of the TFM in legacy style projects.

---
+++ Protocol.FeaturesTests/Test Files/Solutions/SolutionFrameworkOther_Legacy/QAction_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QAction_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("QAction_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8E6489B5-C9EB-4E81-803C-EA5E21ABA2DC")]
+[assembly: AssemblyVersion("1.0.0.0")]
