29ac5cad5f1561bd4a96a0f7e81619f728dea079
Pedro Debevere <pedro.debevere@skyline.be>
2025-03-18 08:28:30 +0100

    Fix for CheckLengtTag check

--- Protocol/Tests/Protocol/Params/Param/Interprete/Length/CheckLengthTag.cs
+++ Protocol/Tests/Protocol/Params/Param/Interprete/Length/CheckLengthTag.cs
@@ -99,7 +99,7 @@ namespace Skyline.DataMiner.CICD.Validators.Protocol.Tests.Protocol.Params.Param
             }
 
             // Length parameters can never be longer than 4 bytes as otherwise it can cause memory corruption in SLPort
-            if (param.Type?.Value == EnumParamType.Length && interprete.Length.Value > 4)
+            if (param.Type?.Value == EnumParamType.Length && interprete?.Length?.Value > 4)
             {
                 results.Add(Error.InvalidValue(test, param, interprete.Length, Severity.Critical, interprete.Length.RawValue, param.Id.RawValue));
             }
