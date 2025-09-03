15897d68fca9bad87530486036aa2862dc08404c
Michiel Oda <michiel.oda@skyline.be>
2025-09-03 10:40:52 +0200

    Update to latest changes for the documentation

--- ProtocolTests/Protocol/Params/Param/CheckSaveAttribute/CheckSaveAttribute.cs
+++ ProtocolTests/Protocol/Params/Param/CheckSaveAttribute/CheckSaveAttribute.cs
@@ -85,7 +85,6 @@ namespace ProtocolTests.Protocol.Params.Param.CheckSaveAttribute
                 FixImpact = FixImpact.NonBreaking,
                 GroupDescription = "",
                 Description = "Unrecommended use of 'save' attribute. Param ID '1001'.",
-                Details = "Having a parameter being both saved and polled from the data-source seems inconsistent. Indeed:" + Environment.NewLine + "- A saved read parameter is typically used for configurations on the DataMiner element side so that user configuration can persist across restarts." + Environment.NewLine + "- A polled parameter will typically never need to be saved as we rely on the fact that the newer value will be polled again shortly after an element restart, no matter if such a parameter is a data parameter or a configuration parameter on the data-source side.",
                 HasCodeFix = false,
             };
 
