97edcfdbf37ef0da2a1c52bc28edcc68876d85aa
SaraSA10 <sara.sabic@skyline.be>
2025-09-19 09:41:40 +0200

    Major Change Checker extension (#180)
    * Major Change Checker extension
    
    * validate handler

---
+++ Skyline.DataMiner.CICD.Tools.Validator/OutputWriters/IMajorChangeCheckerResultWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyline.DataMiner.CICD.Tools.Validator.OutputWriters
+{
+    /// <summary>
+    /// Interface for writing Major Change Checker results.
+    /// </summary>
+    internal interface IMajorChangeCheckerResultWriter
+    {
+        /// <summary>
+        /// Writes the specified MCC results.
+        /// </summary>
+        /// <param name="majorChangeCheckerResults">The MCC results.</param>
+        void WriteResults(MajorChangeCheckerResults majorChangeCheckerResults);
+    }
+}
--- Skyline.DataMiner.CICD.Tools.Validator/Program.cs
+++ Skyline.DataMiner.CICD.Tools.Validator/Program.cs
@@ -1,13 +1,21 @@
 ﻿namespace Skyline.DataMiner.CICD.Tools.Validator
 {
     using System.CommandLine;
+    using System.IO;
+    using System.Reflection;
+    using System.Text.Json;
     using System.Threading.Tasks;
+    using System.Xml.Serialization;
+    using Microsoft.Extensions.Logging;
+    using Skyline.DataMiner.CICD.Tools.Validator.OutputWriters;
 
-    internal class Program
+    public class Program
     {
         public static async Task<int> Main(string[] args)
         {
             var rootCommand = new RootCommand("Validates a DataMiner artifact or solution.");
+
+            #region validate-protocol-solution
 
             var solutionPathOption = new Option<string>(
                 name: "--solution-path",
@@ -80,13 +88,116 @@
                 restoreTimeoutOption
             };
 
-            rootCommand.Add(validateProtocolSolutionCommand);
+            rootCommand.AddCommand(validateProtocolSolutionCommand);
 
             var validatorRunner = new ValidatorRunner();
             validateProtocolSolutionCommand.SetHandler(validatorRunner.ValidateProtocolSolution, solutionPathOption, validatorResultsOutputDirectoryOption, validatorResultsFileNameOption, outputFormatsOption, includeSuppressedOption, performRestoreOption, restoreTimeoutOption);
 
+
+            #endregion
+
+            #region major-change-checker
+
+            var majorChangeCheckerSolutionPathOption = new Option<string>(
+                    name: "--mcc-solution-path",
+                    description: "Path to the new solution file (.sln) of a DataMiner protocol.")
+            {
+                IsRequired = true
+            };
+            majorChangeCheckerSolutionPathOption.LegalFilePathsOnly();
+
+            var oldProtocolPathOption = new Option<string>(
+                name: "--mcc-old-protocol-path",
+                description: "Path to the old protocol.xml file for comparison.")
+            {
+                IsRequired = false
+            };
+            oldProtocolPathOption.LegalFilePathsOnly();
+
+            var majorChangeCheckerOutputDirectoryOption = new Option<string>(
+                name: "--mcc-output-directory",
+                description: "Path to the directory where the MCC results should be stored.")
+            {
+                IsRequired = true
+            };
+
+            var majorChangeCheckerOutputFileNameOption = new Option<string>(
+                name: "--output-file-name",
+                description: "Name of the MCC results file.")
+            {
+                IsRequired = false
+            };
+
+            var majorChangeCheckerOutputFormatsOption = new Option<string[]>(
+                name: "--output-format",
+                description: "Specifies the output format for MCC results. Possible values: JSON, XML, HTML.",
+                getDefaultValue: () => new[] { "JSON", "HTML"}) 
+            {
+                Arity = ArgumentArity.ZeroOrMore,
+                IsRequired = false,
+                AllowMultipleArgumentsPerToken = true,
+            };
+            majorChangeCheckerOutputFormatsOption.FromAmong("JSON", "XML", "HTML");
+
+            var majorChangeCheckerIncludeSuppressedOption = new Option<bool>(
+                name: "--include-suppressed",
+                description: "Specifies whether the suppressed results should also be included in the MCC results.",
+                getDefaultValue: () => false)
+            {
+                IsRequired = false
+            };
+
+            var catalogIdOption = new Option<string>(
+                name: "--catalog-id",
+                description: "Catalog ID for fetching previous protocol version from Catalog API.")
+            {
+                IsRequired = false
+            };
+
+            var catalogApiKeyOption = new Option<string>(
+                name: "--catalog-api-key",
+                description: "Subscription key for Catalog API access.")
+            {
+                IsRequired = false
+            };
+
+            var mccCommand = new Command("major-change-checker", "Performs major change checking between protocol versions.")
+            {
+                majorChangeCheckerSolutionPathOption,
+                oldProtocolPathOption,
+                majorChangeCheckerOutputDirectoryOption,
+                majorChangeCheckerOutputFileNameOption,
+                majorChangeCheckerOutputFormatsOption,
+                majorChangeCheckerIncludeSuppressedOption,
+                catalogIdOption,                
+                catalogApiKeyOption
+            };
+
+            mccCommand.SetHandler(async (context) =>
+            {
+                var solutionPath = context.ParseResult.GetValueForOption(majorChangeCheckerSolutionPathOption);
+                var oldProtocolPath = context.ParseResult.GetValueForOption(oldProtocolPathOption);
+                var outputDirectory = context.ParseResult.GetValueForOption(majorChangeCheckerOutputDirectoryOption);
+                var outputFileName = context.ParseResult.GetValueForOption(majorChangeCheckerOutputFileNameOption);
+                var outputFormats = context.ParseResult.GetValueForOption(majorChangeCheckerOutputFormatsOption);
+                var includeSuppressed = context.ParseResult.GetValueForOption(majorChangeCheckerIncludeSuppressedOption);
+                var catalogId = context.ParseResult.GetValueForOption(catalogIdOption);
+                var apiKey = context.ParseResult.GetValueForOption(catalogApiKeyOption);
+
+                var runner = new MajorChangeCheckerRunner();
+                int result = await runner.RunMajorChangeChecker(
+                    solutionPath, oldProtocolPath, outputDirectory, outputFileName,
+                    outputFormats, includeSuppressed, catalogId, apiKey);
+                
+              
+                context.ExitCode = result;
+            });
+
+            rootCommand.AddCommand(mccCommand);
+            #endregion
+
             int value = await rootCommand.InvokeAsync(args);
             return value;
         }
-    }
-}
+    }     
+}   
