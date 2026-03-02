e1f8d85e50d60379d61447da684f77f06e3242fd
Michiel Oda <michiel.oda@skyline.be>
2026-03-02 10:37:26 +0100

    Update minimum supported DataMiner version

--- Skyline.DataMiner.CICD.Tools.Validator/Helpers/Globals.cs
+++ Skyline.DataMiner.CICD.Tools.Validator/Helpers/Globals.cs
@@ -8,6 +8,6 @@
         /// Gets the minimum DataMiner version with its build number for which support is still given.
         /// </summary>
         /// <value>The minimum DataMiner version with its build number for which support is still given.</value>
-        public static DataMinerVersion MinSupportedDataMinerVersionWithBuildNumber { get; } = DataMinerVersion.Parse("10.3.0.0 - 12752");
+        public static DataMinerVersion MinSupportedDataMinerVersionWithBuildNumber { get; } = DataMinerVersion.Parse("10.4.0.0 - 14003");
     }
 }
