e9c6dc84cb677a72272bde7628f83287468d4509
sofiankerkhof <sofian.kerkhof@skyline.be>
2026-03-05 15:51:15 +0100

    Added Alarm Info check.

---
+++ Documentation/checks/Validator/Protocol/Params/Param/Alarm/Info/CheckInfoTag/Validator_2_79_1.md
@@ -0,0 +1,17 @@
+---
+uid: Validator_2_79_1
+---
+
+# CheckInfoTag
+
+## UnrecommendedInfoTag
+
+<!-- 'Description' and 'Properties' sections are auto-generated. -->
+<!-- DON'T TOUCH ME - I'M USED BY VALIDATOR DOC AUTO-GENERATION CODE -->
+
+<!-- Uncomment to add extra details -->
+<!--### Details-->
+
+<!-- Uncomment to add example code -->
+<!--### Example code-->
+
---
+++ Protocol/Tests/Protocol/Params/Param/Alarm/Info/CheckInfoTag.cs
@@ -0,0 +1,85 @@
+namespace Skyline.DataMiner.CICD.Validators.Protocol.Tests.Protocol.Params.Param.Alarm.Info.CheckInfoTag
+{
+    using System;
+    using System.Collections.Generic;
+    using Skyline.DataMiner.CICD.Models.Protocol.Read;
+    using Skyline.DataMiner.CICD.Validators.Common.Interfaces;
+    using Skyline.DataMiner.CICD.Validators.Common.Model;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common.Attributes;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Common.Extensions;
+    using Skyline.DataMiner.CICD.Validators.Protocol.Interfaces;
+
+    [Test(CheckId.CheckInfoTag, Category.Param)]
+    internal class CheckInfoTag : IValidate, ICodeFix, ICompare
+    {
+        // Please comment out the interfaces that aren't used together with the respective methods.
+
+        public List<IValidationResult> Validate(ValidatorContext context)
+        {
+            List<IValidationResult> results = new List<IValidationResult>();
+
+            foreach (var param in context.EachParamWithValidId())
+            {
+                var alarmTag = param.Alarm;
+
+                // No alarm tag
+                if (alarmTag == null)
+                {
+                    continue;
+                }
+
+                var infoTag = alarmTag.Info;
+
+                // No info tag
+                if (infoTag == null)
+                {
+                    continue;
+                }
+
+                // Info tag should have a value
+                if (infoTag.Value != null)
+                {
+                    var positionNode = infoTag ?? (IReadable)alarmTag;
+                    results.Add(Error.UnrecommendedInfoTag(this, param, positionNode, param.Id.RawValue));
+                }
+            }
+
+            return results;
+        }
+
+        public ICodeFixResult Fix(CodeFixContext context)
+        {
+            CodeFixResult result = new CodeFixResult();
+            if (context.Protocol?.Params == null)
+            {
+                result.Message = "No Param found";
+                return result;
+            }
+
+            switch (context.Result.ErrorId)
+            {
+                case ErrorIds.UnrecommendedInfoTag:
+                    var paramReadNode = (IParamsParam)context.Result.ReferenceNode;
+                    var paramEditNode = context.Protocol.Params.Get(paramReadNode);
+
+                    paramEditNode.Alarm.Info = null;
+                    result.Success = true;
+                    break;
+
+                default:
+                    result.Message = $"This error ({context.Result.ErrorId}) isn't implemented.";
+                    break;
+            }
+
+            return result;
+        }
+        
+        public List<IValidationResult> Compare(MajorChangeCheckContext context)
+        {
+            List<IValidationResult> results = new List<IValidationResult>();
+
+            return results;
+        }
+    }
+}
